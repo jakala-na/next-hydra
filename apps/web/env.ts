23bbe6f102f04cee32dc1e84c0716f56ce588a2a
Alexei Gorobet <alexei.gorobet@jakala.com>
2025-07-16 00:00:57 -0400

    WIP

--- apps/web/env.ts
+++ apps/web/env.ts
@@ -2,6 +2,7 @@ import { keys as cms } from '@repo/cms/keys';
 import { keys as email } from '@repo/email/keys';
 import { keys as flags } from '@repo/feature-flags/keys';
 import { keys as core } from '@repo/next-config/keys';
+import { keys as commerce } from '@repo/commerce/keys';
 import { keys as observability } from '@repo/observability/keys';
 import { keys as rateLimit } from '@repo/rate-limit/keys';
 import { keys as security } from '@repo/security/keys';
@@ -10,6 +11,7 @@ import { createEnv } from '@t3-oss/env-nextjs';
 export const env = createEnv({
   extends: [
     cms(),
+    commerce(),
     core(),
     email(),
     observability(),
