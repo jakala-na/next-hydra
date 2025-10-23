3664ceca6fb2ed1ba87c09b0b5016cf9feca4669
Nathan Plowman <nathan.plowman@ffw.com>
2025-10-23 15:35:15 -0600

    Fixed code sniffer errors in component renderer.

--- packages/cms/components/component-renderer.tsx
+++ packages/cms/components/component-renderer.tsx
@@ -9,24 +9,30 @@
  * 4. It should be able to render the component(s) with the data you provided.
  * 5. It should skip rendering if component is not found in the componentMap.
  */
-import { HeroSection } from './blocks/hero-section';
-import { ComponentInstance } from '@uniformdev/canvas';
+
+import type { Locale } from "@repo/i18n";
+import type { ComponentInstance } from "@uniformdev/canvas";
+import { HeroSection } from "./blocks/hero-section";
 
 export const componentMap = {
   heroSection: HeroSection,
 } as const;
 
-export default async function ComponentRenderer({ components, locale }: { components: ComponentInstance[]; locale: string }) {
-  'use server';
+export default function ComponentRenderer({
+  components,
+  locale,
+}: {
+  components: ComponentInstance[];
+  locale: Locale;
+}) {
+  "use server";
 
   const renderedItems = components.map((item) => {
     const Component = componentMap[item.type as keyof typeof componentMap];
-    if (!Component) return null;
+    if (!Component) {
+      return null;
+    }
     return <Component key={item._id} data={item.parameters} locale={locale} />;
-  })
-  return (
-    <div>
-      {renderedItems}
-    </div>
-  );
+  });
+  return <div>{renderedItems}</div>;
 }
