760af27feb7d4fc17bae7e0c8cf7d22afdb962a6
Nathan Plowman <nathan.plowman@ffw.com>
2025-10-23 15:19:20 -0600

    Fixed import in hero section component.

--- packages/cms/components/blocks/hero-section.tsx
+++ packages/cms/components/blocks/hero-section.tsx
@@ -1,7 +1,7 @@
 import { HeroSection as HeroSectionComponent } from "@repo/design-system/components/cms/blocks/hero-section";
 import { renderToHtml } from "@uniformdev/richtext";
 import  { transformLocale } from "@repo/cms/lib/utils/transform-locale";
-import type { Locale } from "@repo/cms/i18n/locales";
+import type { Locale } from "@repo/i18n";
 
 export function HeroSection(
   props: {
@@ -17,7 +17,6 @@ export function HeroSection(
     width: data.image.locales[localeCode][0].fields.width.value,
     height: data.image.locales[localeCode][0].fields.height.value,
   };
-  console.log('data.description', data.description.locales[localeCode]);
   const title = props.data.title.locales[localeCode] ?? '';
   const tagline = props.data.tagline.locales[localeCode] ?? '';
   const description = renderToHtml(props.data.description.locales[localeCode]);
