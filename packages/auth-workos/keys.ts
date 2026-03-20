242aa36ade2b8685d3aa7902f51028374c3b2272
andreyjan <chemberdjian@gmail.com>
2026-03-20 17:57:50 +0200

    Simplify initial install

--- packages/auth-workos/keys.ts
+++ packages/auth-workos/keys.ts
@@ -6,12 +6,15 @@ const MIN_COOKIE_PASSWORD_LENGTH = 32;
 export const keys = () =>
   createEnv({
     server: {
-      WORKOS_API_KEY: z.string().startsWith("sk_"),
-      WORKOS_CLIENT_ID: z.string().startsWith("client_"),
-      WORKOS_COOKIE_PASSWORD: z.string().min(MIN_COOKIE_PASSWORD_LENGTH),
+      WORKOS_API_KEY: z.string().startsWith("sk_").optional(),
+      WORKOS_CLIENT_ID: z.string().startsWith("client_").optional(),
+      WORKOS_COOKIE_PASSWORD: z
+        .string()
+        .min(MIN_COOKIE_PASSWORD_LENGTH)
+        .optional(),
     },
     client: {
-      NEXT_PUBLIC_WORKOS_REDIRECT_URI: z.string().url(),
+      NEXT_PUBLIC_WORKOS_REDIRECT_URI: z.string().url().optional(),
     },
     runtimeEnv: {
       WORKOS_API_KEY: process.env.WORKOS_API_KEY,
